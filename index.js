5175cae0afc629a54095bc3f9f4d0b8c08cda57d
copsmen809 <copsmen809@gmail.com>
2026-05-20 00:14:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ export default {
     const url = new URL(request.url);
     const pathname = url.pathname; 
 
-    // Regex untuk mengambil ID video (misal dari /pkbievy0jp33.mp4 jadi pkbievy0jp33)
+    // Regex tetap sama, ngambil ID sebelum ".mp4"
     const match = pathname.match(/\/([^\/.]+)\.mp4$/);
 
     if (match) {
       const videoId = match[1]; 
-      // Link dasar dari Vidoy sesuai screenshot yang lu kasih
-      const targetUrl = 'https://vidzp.com/e/${videoId}`;
+      
+      // Kita pakai domain awal mereka yang stabil buat nge-redirect
+      const targetUrl = `https://mp4abc.de/e/${videoId}`;
 
-      // Redirect ke link stream asli
+      // Biarkan sistem mp4abc.de yang pusing mikirin domain embed akhirnya
       return Response.redirect(targetUrl, 301);
     }
 
-    // Response default kalau link-nya gak pake .mp4
-    return new Response('Sistem Siap! Gunakan format: cdn2.vid7me.id/ID_VIDEO.mp4', {
-      headers: { 'content-type': 'text/plain' },
+    return new Response('Sistem Aktif! Format URL: cdn2.vid7me.id/ID_VIDEO.mp4', {
+      headers: { 'content-type': 'text/plain; charset=utf-8' },
     });
   },
 };
