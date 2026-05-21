d9999a22b5818b5ed9091c0d2174b81440528c5e
copsmen809 <copsmen809@gmail.com>
2026-05-21 19:00:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ export default {
     const url = new URL(request.url);
     const pathname = url.pathname;
 
-    // Jika mengakses root/halaman utama cdn2.slirpdrive.com
+    // Jika mengakses halaman utama (https://cdn2.slirpdrive.com/)
     if (pathname === '/' || pathname === '') {
-      return new Response('CDN Embed Mirror Service is Active.', {
+      return new Response('cdn2.slirpdrive.com - CDN Embed Mirror Service is Active.', {
         status: 200,
         headers: { 'content-type': 'text/plain' }
       });
     }
 
-    // Langsung arahkan path utuh beserta ekstensinya ke cdn2.aceimg.com
+    // Mengarahkan path video utuh ke server cdn2.aceimg.com
     // Contoh: cdn2.slirpdrive.com/16fca76a2.mp4 -> cdn2.aceimg.com/16fca76a2.mp4
     const targetUrl = `https://cdn2.aceimg.com${pathname}${url.search}`;
 
-    // Redirect 302 agar langsung mengarah ke video embed asli
+    // Lakukan redirect 302 ke video embed aceimg
     return Response.redirect(targetUrl, 302);
   },
 };
