3ad36bd1429969d1f4233ad16d322073c22246b6
copsmen809 <copsmen809@gmail.com>
2026-05-22 09:05:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ export default {
 
     // Jika mengakses halaman utama (https://cdn2.slirpdrive.com/)
     if (pathname === '/' || pathname === '') {
-      return new Response('cdn2.slirpdrive.com - CDN Embed Mirror Service is Active.', {
+      return new Response('cdn2.slirpdrive.com - CDN Mirror Service is Active.', {
         status: 200,
         headers: { 'content-type': 'text/plain' }
       });
     }
 
-    // Mengarahkan path video utuh ke server cdn2.aceimg.com
-    // Contoh: cdn2.slirpdrive.com/16fca76a2.mp4 -> cdn2.aceimg.com/16fca76a2.mp4
-    const targetUrl = `https://cdn2.aceimg.com${pathname}${url.search}`;
+    // Ambil nama file beserta ekstensinya (menghilangkan tanda "/" di depan)
+    // Contoh: "/16fca76a2.mp4" menjadi "16fca76a2.mp4"
+    const fileName = pathname.substring(1);
 
-    // Lakukan redirect 302 ke video embed aceimg
+    // Gabungkan ke format default aceimg.com dengan parameter ?f=
+    // Hasil: https://aceimg.com/upload/?f=16fca76a2.mp4
+    const targetUrl = `https://aceimg.com/upload/?f=${fileName}`;
+
+    // Lakukan redirect 302 ke halaman default aceimg
     return Response.redirect(targetUrl, 302);
   },
 };
